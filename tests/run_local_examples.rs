e4827bcb49b06f2de5abab561a164636f0fc8917
Luca Visentin <luca.visentin@unito.it>
2024-02-18 19:22:34 +0100

    feat: add failing examples

--- tests/run_local_examples.rs
+++ tests/run_local_examples.rs
@@ -3,7 +3,7 @@ use paste;
 use std::collections::HashMap;
 use std::env::set_current_dir;
 use std::mem::drop;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::{fs, io};
 use tempfile::TempDir;
@@ -65,11 +65,32 @@ fn snapshot(dir: &PathBuf) -> io::Result<Content> {
     Ok(content)
 }
 
-fn check_identical(snapshot: Content) -> io::Result<()> {
-    for (path, data) in snapshot {
+fn two_way_check<T>(a: &Vec<T>, b: &Vec<T>) -> bool
+where
+    T: PartialEq,
+{
+    a.iter().all(|item| b.contains(item)) & b.iter().all(|item| a.contains(item))
+}
+
+fn check_identical(old_snap: Content, target: impl AsRef<Path>) -> io::Result<()> {
+    let new_snap = snapshot(&target.as_ref().to_owned().to_path_buf()).unwrap();
+
+    let old_files: Vec<PathBuf> = old_snap.keys().cloned().collect();
+    let new_files: Vec<PathBuf> = new_snap.keys().cloned().collect();
+    if !two_way_check(&old_files, &new_files) {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!(
+                "Number of files differ: old: {:?}, new: {:?}",
+                old_files, new_files
+            ),
+        ));
+    }
+
+    for path in old_files {
         let current = fs::read(&path)?;
 
-        if current != data {
+        if current != *old_snap.get(&path).unwrap() {
             return Err(io::Error::new(
                 io::ErrorKind::InvalidData,
                 format!("File {:?} has modified data!", path),
@@ -85,7 +106,7 @@ macro_rules! run_example_named {
         paste::item! {
 
             #[test]
-            fn [< run_test_example_ $name >] () {
+            fn [< example_ $name >] () {
                 let repo = setup_test();
                 let target = repo.path().join(format!("examples/{}", $name));
 
@@ -110,7 +131,49 @@ macro_rules! run_example_named {
 
                 assert!(code.status.success());
 
-                assert!(check_identical(snap).is_ok());
+                assert!(check_identical(snap, &target).is_ok());
+
+                // The 'repo' variable gets dropped here, so the tempdir is cleaned up
+                // SPECIFICALLY here.
+                drop(repo);
+            }
+        }
+    };
+}
+
+// TODO: This is repeated from above with only small differences.
+// Can we avaoid it?
+macro_rules! run_failing_example_named {
+    ($name:literal) => {
+        paste::item! {
+
+            #[test]
+            fn [< failing_example_ $name >] () {
+                let repo = setup_test();
+                let target = repo.path().join(format!("examples/{}", $name));
+
+                // Move into the example
+                println!("{:?}", target);
+                set_current_dir(&target).unwrap();
+
+                // Take a snapshot of the contents of the repo
+                let snap = snapshot(&target).expect("Could not take snapshot of repo");
+
+                // Run the things
+                let code = Command::new("bash")
+                    .args([target.join("run")])
+                    .output()
+                    .expect("Could not collect child output");
+
+                eprintln!(
+                    "Command Output\nSTDOUT:\n{}\n\nSTDERR:\n{}",
+                    String::from_utf8_lossy(&code.stdout),
+                    String::from_utf8_lossy(&code.stderr),
+                );
+
+                assert!(! code.status.success());
+
+                assert!(check_identical(snap, &target).is_ok());
 
                 // The 'repo' variable gets dropped here, so the tempdir is cleaned up
                 // SPECIFICALLY here.
@@ -122,3 +185,4 @@ macro_rules! run_example_named {
 
 run_example_named!("basic_pipes");
 run_example_named!("basic_containers");
+run_failing_example_named!("safe_failure");
